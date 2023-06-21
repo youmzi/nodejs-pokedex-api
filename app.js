866153936bc6b63a1a2169be69a49f74ad31fd8e
youmzi <youssef.meziani@outlook.fr>
2023-06-21 23:17:30 +0200

    Premier commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const morgan = require('morgan')
+const favicon = require('serve-favicon')
+const bodyParser = require('body-parser')
+const sequelize = require('./src/db/sequelize')
+
+const app = express()
+const port = 3000
+
+
+
+app
+    .use(favicon(__dirname + '/favicon.ico'))
+    .use(morgan('dev'))
+    .use(bodyParser.json())
+
+sequelize.initDb()
+
+require('./src/routes/findAllPokemons')(app)
+require('./src/routes/findPokemonByPk')(app)
+require('./src/routes/createPokemon')(app)
+require('./src/routes/updatePokemon')(app)
+require('./src/routes/deletePokemon')(app)
+
+app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
