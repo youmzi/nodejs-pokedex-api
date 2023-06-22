8796c1171d18a82f24dc61d0325b4cddf1899d42
youmzi <youssef.meziani@outlook.fr>
2023-06-22 20:48:49 +0200

    Feat : Gestion des erreurs d'authentification

---
+++ src/routes/login.js
@@ -0,0 +1,26 @@
+const { User } = require('../db/sequelize')
+const bcrypt = require('bcrypt')
+  
+module.exports = (app) => {
+    app.post('/api/login', (req, res) => {
+        User.findOne({ where: { username: req.body.username } }).then(user => {
+
+            if(!user) {
+                const message = `L'utilisateur demandé n'existe pas.`
+                return res.status(404).json({ message })
+            }
+            bcrypt.compare(req.body.password, user.password).then(isPasswordValid => {
+                if(!isPasswordValid) {
+                    const message = `Le mot de passe est incorrect`;
+                    return res.status(401).json({ message })
+                }
+                const message = `L'utilisateur a été connecté avec succès`;
+                return res.json({ message, data: user })
+            })
+        })
+        .catch(error => {
+            const message = `L'utilisateur n'a pas pu être connecté. Réessayez dans quelques instants.`;
+            return res.json({ message, data: error })
+        })
+    })
+}
