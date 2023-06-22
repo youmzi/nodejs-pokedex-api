165a488b80a101719e0db864713899c60aad4b44
youmzi <youssef.meziani@outlook.fr>
2023-06-22 22:03:03 +0200

    Feat : Sécurisation des endpoints avec l'authentification par token

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,10 +1,11 @@
 const { User } = require('../db/sequelize')
 const bcrypt = require('bcrypt')
-  
+const jwt = require('jsonwebtoken')
+const privateKey = require('../auth/private_key')
+
 module.exports = (app) => {
     app.post('/api/login', (req, res) => {
         User.findOne({ where: { username: req.body.username } }).then(user => {
-
             if(!user) {
                 const message = `L'utilisateur demandé n'existe pas.`
                 return res.status(404).json({ message })
@@ -14,8 +15,16 @@ module.exports = (app) => {
                     const message = `Le mot de passe est incorrect`;
                     return res.status(401).json({ message })
                 }
+
+                // JWT
+                const token = jwt.sign(
+                    { userId: user.id },
+                    privateKey,
+                    { expiresIn: '24h' }
+                )
+
                 const message = `L'utilisateur a été connecté avec succès`;
-                return res.json({ message, data: user })
+                return res.json({ message, data: user, token })
             })
         })
         .catch(error => {
