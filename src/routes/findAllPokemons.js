465dca4f63f81ba979eaa07df707f88debfc0d28
youmzi <youssef.meziani@outlook.fr>
2023-06-22 18:43:29 +0200

    Feat : Mise en place de la recherche des Pokémons selon des critères

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,15 +1,40 @@
 const { Pokemon } = require('../db/sequelize')
-  
+const { Op } = require('sequelize')
+
 module.exports = (app) => {
 	app.get('/api/pokemons', (req, res) => {
-    	Pokemon.findAll()
-		.then(pokemons => {
-			const message = 'La liste des pokémons a bien été récupérée.'
-			res.json({ message, data: pokemons })
-		})
-		.catch(error => {
-			const message = `La liste des Pokémons n'a pas pu être récupérée. Réessayez dans quelques instants.`
-			res.status(500).json({ message, data: error })
-		})
+		if(req.query.name) {
+			const name = req.query.name
+			const limit = parseInt(req.query.limit) || 5
+
+			if(name.length < 2) {
+				const message = `Le terme de recherche doit contenir au moins 2 caractères.`
+				return res.status(400).json({ message })
+			}
+
+			return Pokemon.findAndCountAll({
+				where: {
+					name: { // 'name' est la propriété du modèle pokémon
+						[Op.like]: `%${name}%` //'name' est le critère de recherche
+					}
+				},
+				order: ['name'],
+				limit: limit
+			})
+			.then(({count, rows}) => {
+				const message = `Il y a ${count} Pokémon(s) qui correspondent au terme de recherche ${name}`
+				res.json({ message, data: rows })
+			})
+		} else {
+			Pokemon.findAll({ order: ['name'] })
+			.then(pokemons => {
+				const message = 'La liste des pokémons a bien été récupérée.'
+				res.json({ message, data: pokemons })
+			})
+			.catch(error => {
+				const message = `La liste des Pokémons n'a pas pu être récupérée. Réessayez dans quelques instants.`
+				res.status(500).json({ message, data: error })
+			})
+		}
   	})
 }
