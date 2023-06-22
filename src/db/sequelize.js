e011918e438be5b4f9b1c30b0a00ff5c65014127
youmzi <youssef.meziani@outlook.fr>
2023-06-22 19:16:27 +0200

    Feat : Réalisation du model User

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -1,33 +1,42 @@
 const { Sequelize, DataTypes } = require('sequelize')
 const PokemonModel = require('../models/pokemon')
+const UserModel = require('../models/user')
 const pokemons = require('./mock-pokemon')
   
 const sequelize = new Sequelize('pokedex', 'root', '', {
-  host: 'localhost',
-  dialect: 'mariadb',
-  dialectOptions: {
-    timezone: 'Etc/GMT-2',
-  },
-  logging: false
+	host: 'localhost',
+	dialect: 'mariadb',
+	dialectOptions: {
+		timezone: 'Etc/GMT-2',
+	},
+  	logging: false
 })
   
 const Pokemon = PokemonModel(sequelize, DataTypes)
+const User = UserModel(sequelize, DataTypes)
   
 const initDb = () => {
-  return sequelize.sync({force: true}).then(_ => {
-    pokemons.map(pokemon => {
-      Pokemon.create({
-        name: pokemon.name,
-        hp: pokemon.hp,
-        cp: pokemon.cp,
-        picture: pokemon.picture,
-        types: pokemon.types
-      }).then(pokemon => console.log(pokemon.toJSON()))
-    })
-    console.log('La base de donnée a bien été initialisée !')
-  })
+	return sequelize.sync({force: true}).then(_ => {
+		pokemons.map(pokemon => {
+	  		Pokemon.create({
+				name: pokemon.name,
+				hp: pokemon.hp,
+				cp: pokemon.cp,
+				picture: pokemon.picture,
+				types: pokemon.types
+	  		}).then(pokemon => console.log(pokemon.toJSON()))
+		})
+
+		User.create({
+			username: 'pikachu',
+			password: 'pikachu'
+		})
+		.then(user => console.log(user.toJSON()))
+
+		console.log('La base de donnée a bien été initialisée !')
+	})
 }
   
 module.exports = { 
-  initDb, Pokemon
+  initDb, Pokemon, User
 }
---
+++ src/models/user.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('User', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        unique : {
+            msg: 'Le nom d\'utilisateur existe déjà.'
+        }
+      },
+      password: {
+        type: DataTypes.STRING
+      }
+    },
+    {
+        createdAt: 'created',
+        updatedAt: false
+    })
+  }
