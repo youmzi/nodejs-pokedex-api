473b8cd998e04b3ab9aa4167cd4ae7800d8b08f1
youmzi <youssef.meziani@outlook.fr>
2023-06-22 23:51:40 +0200

    Build : Suppression packages

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require('express')
-const morgan = require('morgan')
 const favicon = require('serve-favicon')
 const bodyParser = require('body-parser')
 const sequelize = require('./src/db/sequelize')
 
 const app = express()
-const port = 3000
-
-
+const port = process.env.PORT || 3000
 
 app
     .use(favicon(__dirname + '/favicon.ico'))
-    .use(morgan('dev'))
     .use(bodyParser.json())
 
 sequelize.initDb()
+
+app.get('/', (req, res) => {
+    res.json('Hello, Heroku !')
+})
 
 require('./src/routes/findAllPokemons')(app)
 require('./src/routes/findPokemonByPk')(app)
@@ -23,7 +23,6 @@ require('./src/routes/updatePokemon')(app)
 require('./src/routes/deletePokemon')(app)
 require('./src/routes/login')(app)
 
-// On ajoute la gestion des erreurs 404
 app.use(({res}) => {
     const message = "Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL."
     res.status(404).json({message})
