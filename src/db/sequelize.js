64a77857ce85191678f6c07e9f0541719f36ace1
youmzi <youssef.meziani@outlook.fr>
2023-06-23 01:13:06 +0200

    Feat : Configuration base de données

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -3,21 +3,33 @@ const PokemonModel = require('../models/pokemon')
 const UserModel = require('../models/user')
 const pokemons = require('./mock-pokemon')
 const bcrypt = require('bcrypt')
-  
-const sequelize = new Sequelize('pokedex', 'root', '', {
-	host: 'localhost',
-	dialect: 'mariadb',
-	dialectOptions: {
-		timezone: 'Etc/GMT-2',
-	},
-  	logging: false
-})
+
+let sequelize
+if(process.env.NODE_ENV === 'production') {
+	sequelize = new Sequelize('jaj0nh3xbl0e5ofu', 'e39bohpa8cfszepx', 'j2qabbmguqfab00r', {
+		host: 'q0h7yf5pynynaq54.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+		dialect: 'mariadb',
+		dialectOptions: {
+			timezone: 'Etc/GMT-2',
+		},
+		logging: true
+	})
+} else {
+	sequelize = new Sequelize('pokedex', 'root', '', {
+		host: 'localhost',
+		dialect: 'mariadb',
+		dialectOptions: {
+			timezone: 'Etc/GMT-2',
+		},
+		logging: false
+	})
+}
   
 const Pokemon = PokemonModel(sequelize, DataTypes)
 const User = UserModel(sequelize, DataTypes)
   
 const initDb = () => {
-	return sequelize.sync({force: true}).then(_ => {
+	return sequelize.sync().then(_ => {
 		pokemons.map(pokemon => {
 	  		Pokemon.create({
 				name: pokemon.name,
